a040b97f063edcc141ae6910c9378594f6ad4174
Sudip Subedi <sudip.subedi99@gmail.com>
2024-09-19 00:28:48 +0545

    type function and basic concepts

---
+++ 01_Class.py
@@ -0,0 +1,21 @@
+a = 5
+print(type(a))
+
+mylist1 = [1,2,3,4,5]
+print(mylist1[1])
+print(mylist1)
+print(type(mylist1))
+
+mylist2 = list()
+print(mylist2)
+print(type(mylist2))
+
+mylist3 = list([1,2,3])
+print(mylist3)
+
+# data type is a class
+# variable of a data type is an obejct
+# class name is in PascalCase
+# Varibale name is in snake_case
+# private data members are denoted by -
+# public member functions are denoted by +
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+st = [1,2,3,4,5]
+# print(type(list))
